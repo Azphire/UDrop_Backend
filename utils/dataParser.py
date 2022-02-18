552a1a31a111508f436565a8d09106d4a4ad4385
Azphire <Azphire@163.com>
2022-02-18 22:25:12 +0800

    继续继续改bug

--- utils/dataParser.py
+++ utils/dataParser.py
@@ -5,13 +5,32 @@ import datetime
 class user:
     def __init__(self, data):
         self.userId = int(data[0])
+
         self.name = data[1]
-        self.vip = int(data[2])
-        self.gender = int(data[3])
-        self.age = int(data[4])
-        self.audioType = int(data[5])
-        self.speed = int(data[6])
+
+        self.vip = data[2]
+        if self.vip:
+            self.vip = int(self.vip)
+
+        self.gender = data[3]
+        if self.gender:
+            self.gender = int(self.gender)
+
+        self.age = data[4]
+        if self.age:
+            self.age = int(self.age)
+
+        self.audioType = data[5]
+        if self.audioType:
+            self.audioType = int(self.audioType)
+
+        self.speed = data[6]
+        if self.speed:
+            self.speed = int(self.speed)
+
         # self.password = data[7]
+
         self.motto = data[8]
+
         current = datetime.datetime.now()
         self.learnedDays = (current - data[9]).days + 1
